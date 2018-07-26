5053cb51ba6ca85e0a0a6d37b4ad215f9277dd4e
Hamish Cumming <hamish.cumming@gmail.com>
2018-07-26 14:25:21 +1200

    fix for svg text layout in ie11

--- src/sunburst.js
+++ src/sunburst.js
@@ -18,27 +18,27 @@ export default Kapsule({
   props: {
     width: { default: window.innerWidth },
     height: { default: window.innerHeight },
-    data: { onChange: function() { this._parseData(); }},
-    children: { default: 'children', onChange: function() { this._parseData(); }},
-    sort: { onChange: function() { this._parseData(); }},
+    data: { onChange: function () { this._parseData(); } },
+    children: { default: 'children', onChange: function () { this._parseData(); } },
+    sort: { onChange: function () { this._parseData(); } },
     label: { default: d => d.name },
     size: {
       default: 'value',
-      onChange: function() { this._parseData(); }
+      onChange: function () { this._parseData(); }
     },
     color: { default: d => 'lightgrey' },
     minSliceAngle: { default: .2 },
     showLabels: { default: true },
     tooltipContent: { default: d => '', triggerUpdate: false },
     focusOnNode: {
-      onChange: function(d, state) {
+      onChange: function (d, state) {
         if (d && state.initialised) {
           moveStackToFront(d);
         }
 
         function moveStackToFront(elD) {
           state.svg.selectAll('.slice').filter(d => d === elD)
-            .each(function(d) {
+            .each(function (d) {
               this.parentNode.appendChild(this);
               if (d.parent) { moveStackToFront(d.parent); }
             })
@@ -49,7 +49,7 @@ export default Kapsule({
   },
 
   methods: {
-    _parseData: function(state) {
+    _parseData: function (state) {
       if (state.data) {
         const hierData = d3Hierarchy(state.data, accessorFn(state.children))
           .sum(accessorFn(state.size));
@@ -66,192 +66,197 @@ export default Kapsule({
         var fnode = this.focusOnNode();
         var me = this; //TODO: must be a better way than this?
         if (fnode != null) {
-            hierData.descendants().forEach(function (d, i) {
-                var temp = accessorFn(state.label)(d.data);
-                if (accessorFn(state.label)(d.data) == accessorFn(state.label)(fnode.data)) {
-                    me.focusOnNode(d);
-                }
-            });
+          hierData.descendants().forEach(function (d, i) {
+            var temp = accessorFn(state.label)(d.data);
+            if (accessorFn(state.label)(d.data) == accessorFn(state.label)(fnode.data)) {
+              me.focusOnNode(d);
+            }
+          });
 
-        state.layoutData = hierData.descendants();
+          state.layoutData = hierData.descendants();
+        }
       }
-    }
-  },
+    },
 
-  init: function(domNode, state) {
-    state.chartId = Math.round(Math.random() * 1e12); // Unique ID for DOM elems
+    init: function (domNode, state) {
+      state.chartId = Math.round(Math.random() * 1e12); // Unique ID for DOM elems
 
-    state.radiusScale = scaleSqrt();
-    state.angleScale = scaleLinear()
-      .domain([0, 10]) // For initial build-in animation
-      .range([0, 2 * Math.PI])
-      .clamp(true);
+      state.radiusScale = scaleSqrt();
+      state.angleScale = scaleLinear()
+        .domain([0, 10]) // For initial build-in animation
+        .range([0, 2 * Math.PI])
+        .clamp(true);
 
-    state.arc = d3Arc()
-      .startAngle(d => state.angleScale(d.x0))
-      .endAngle(d => state.angleScale(d.x1))
-      .innerRadius(d => Math.max(0, state.radiusScale(d.y0)))
-      .outerRadius(d => Math.max(0, state.radiusScale(d.y1)));
+      state.arc = d3Arc()
+        .startAngle(d => state.angleScale(d.x0))
+        .endAngle(d => state.angleScale(d.x1))
+        .innerRadius(d => Math.max(0, state.radiusScale(d.y0)))
+        .outerRadius(d => Math.max(0, state.radiusScale(d.y1)));
 
-    const el = d3Select(domNode)
-      .append('div').attr('class', 'sunburst-viz');
+      const el = d3Select(domNode)
+        .append('div').attr('class', 'sunburst-viz');
 
-    state.svg = el.append('svg');
-    state.canvas = state.svg.append('g');
+      state.svg = el.append('svg');
+      state.canvas = state.svg.append('g');
 
-    // tooltips
-    state.tooltip = d3Select('body')
-      .append('div')
-          .attr('class', 'sunburst-tooltip');
+      // tooltips
+      state.tooltip = d3Select('body')
+        .append('div')
+        .attr('class', 'sunburst-tooltip');
 
-    // tooltip cleanup on unmount
-    domNode.addEventListener ('DOMNodeRemoved', function(e) {
-      if (e.target === this) { state.tooltip.remove(); }
-    });
+      // tooltip cleanup on unmount
+      domNode.addEventListener('DOMNodeRemoved', function (e) {
+        if (e.target === this) { state.tooltip.remove(); }
+      });
 
-    state.canvas.on('mousemove', () => {
-      state.tooltip
-        .style('left', d3Event.pageX + 'px')
-        .style('top', d3Event.pageY + 'px');
-    });
+      state.canvas.on('mousemove', () => {
+        state.tooltip
+          .style('left', d3Event.pageX + 'px')
+          .style('top', d3Event.pageY + 'px');
+      });
 
-    // Reset focus by clicking on canvas
-    state.svg.on('click', () => this.focusOnNode(null));
-  },
+      // Reset focus by clicking on canvas
+      state.svg.on('click', () => this.focusOnNode(null));
+    },
 
-  update: function(state) {
-    const maxRadius = (Math.min(state.width, state.height) / 2);
-    state.radiusScale.range([maxRadius * .1, maxRadius]);
+    update: function (state) {
+      const maxRadius = (Math.min(state.width, state.height) / 2);
+      state.radiusScale.range([maxRadius * .1, maxRadius]);
 
-    state.svg
-      .style('width', state.width + 'px')
-      .style('height', state.height + 'px')
-      .attr('viewBox', `${-state.width/2} ${-state.height/2} ${state.width} ${state.height}`);
+      state.svg
+        .style('width', state.width + 'px')
+        .style('height', state.height + 'px')
+        .attr('viewBox', `${-state.width / 2} ${-state.height / 2} ${state.width} ${state.height}`);
 
-    if (!state.layoutData) return;
+      if (!state.layoutData) return;
 
-    const focusD = state.focusOnNode || { x0: 0, x1: 1, y0: 0, y1: 1 };
+      const focusD = state.focusOnNode || { x0: 0, x1: 1, y0: 0, y1: 1 };
 
-    const slice = state.canvas.selectAll('.slice')
-      .data(
+      const slice = state.canvas.selectAll('.slice')
+        .data(
         state.layoutData
           .filter(d => // Show only slices with a large enough angle
             d.x1 >= focusD.x0
             && d.x0 <= focusD.x1
-            && (d.x1-d.x0)/(focusD.x1-focusD.x0) > state.minSliceAngle/360
+            && (d.x1 - d.x0) / (focusD.x1 - focusD.x0) > state.minSliceAngle / 360
           ),
         d => d.id
-      );
+        );
 
-    const nameOf = accessorFn(state.label);
-    const colorOf = accessorFn(state.color);
-    const transition = d3Transition().duration(TRANSITION_DURATION);
+      const nameOf = accessorFn(state.label);
+      const colorOf = accessorFn(state.color);
+      const transition = d3Transition().duration(TRANSITION_DURATION);
 
-    // Apply zoom
-    state.svg.transition(transition)
-      .tween('scale', () => {
-        const xd = d3Interpolate(state.angleScale.domain(), [focusD.x0, focusD.x1]);
-        const yd = d3Interpolate(state.radiusScale.domain(), [focusD.y0, 1]);
-        return t => {
-          state.angleScale.domain(xd(t));
-          state.radiusScale.domain(yd(t));
-        };
-      });
+      // Apply zoom
+      state.svg.transition(transition)
+        .tween('scale', () => {
+          const xd = d3Interpolate(state.angleScale.domain(), [focusD.x0, focusD.x1]);
+          const yd = d3Interpolate(state.radiusScale.domain(), [focusD.y0, 1]);
+          return t => {
+            state.angleScale.domain(xd(t));
+            state.radiusScale.domain(yd(t));
+          };
+        });
 
-    // Exiting
-    const oldSlice = slice.exit().transition(transition).style('opacity', 0).remove();
-    oldSlice.select('path.main-arc').attrTween('d', d => () => state.arc(d));
-    oldSlice.select('path.hidden-arc').attrTween('d', d => () => middleArcLine(d));
+      // Exiting
+      const oldSlice = slice.exit().transition(transition).style('opacity', 0).remove();
+      oldSlice.select('path.main-arc').attrTween('d', d => () => state.arc(d));
+      oldSlice.select('path.hidden-arc').attrTween('d', d => () => middleArcLine(d));
 
-    // Entering
-    const newSlice = slice.enter().append('g')
-      .attr('class', 'slice')
-      .style('opacity', 0)
-      .on('click', d => {
-        d3Event.stopPropagation();
-        (state.onNodeClick || this.focusOnNode)(d);
-      })
-      .on('mouseover', d => {
-        state.tooltip.style('display', 'inline');
-        state.tooltip.html(`<div class="tooltip-title">${
-          getNodeStack(d).map(d => nameOf(d.data)).join(' > ')
-        }</div>${state.tooltipContent(d.data, d)}`);
-      })
-      .on('mouseout', () => { state.tooltip.style('display', 'none'); });
+      // Entering
+      const newSlice = slice.enter().append('g')
+        .attr('class', 'slice')
+        .style('opacity', 0)
+        .on('click', d => {
+          d3Event.stopPropagation();
+          (state.onNodeClick || this.focusOnNode)(d);
+        })
+        .on('mouseover', d => {
+          state.tooltip.style('display', 'inline');
+          state.tooltip.html(`<div class="tooltip-title">${
+            getNodeStack(d).map(d => nameOf(d.data)).join(' > ')
+            }</div>${state.tooltipContent(d.data, d)}`);
+        })
+        .on('mouseout', () => { state.tooltip.style('display', 'none'); });
 
-    newSlice.append('path')
-      .attr('class', 'main-arc')
-      .style('fill', d => colorOf(d.data, d.parent));
+      newSlice.append('path')
+        .attr('class', 'main-arc')
+        .style('fill', d => colorOf(d.data, d.parent));
 
-    newSlice.append('path')
-      .attr('class', 'hidden-arc')
-      .attr('id', d => `hidden-arc-${state.chartId}-${d.id}`);
+      newSlice.append('path')
+        .attr('class', 'hidden-arc')
+        .attr('id', d => `hidden-arc-${state.chartId}-${d.id}`);
 
-    const label = newSlice.append('text')
+      const label = newSlice.append('text')
         .attr('class', 'path-label');
 
-    // Add white contour
-    label.append('textPath')
-      .attr('class', 'text-contour')
-      .attr('startOffset','50%')
-      .attr('xlink:href', d => `#hidden-arc-${state.chartId}-${d.id}` );
+      //IE11 does not go well with multiple textpaths per text
+      const label2 = newSlice.append('text')
+        .attr('class', 'path-label');
 
-    label.append('textPath')
-      .attr('startOffset','50%')
-      .attr('xlink:href', d => `#hidden-arc-${state.chartId}-${d.id}` );
+      // Add white contour
+      label.append('textPath')
+        .attr('class', 'text-contour')
+        .attr('startOffset', '50%')
+        .attr('xlink:href', d => `#hidden-arc-${state.chartId}-${d.id}`);
 
-    // Entering + Updating
-    const allSlices = slice.merge(newSlice);
+      label2.append('textPath')
+        .attr('startOffset', '50%')
+        .attr('xlink:href', d => `#hidden-arc-${state.chartId}-${d.id}`);
 
-    allSlices.style('opacity', 1);
+      // Entering + Updating
+      const allSlices = slice.merge(newSlice);
 
-    allSlices.select('path.main-arc').transition(transition)
-      .attrTween('d', d => () => state.arc(d))
-      .style('fill', d => colorOf(d.data, d.parent));
+      allSlices.style('opacity', 1);
 
-    allSlices.select('path.hidden-arc').transition(transition)
-      .attrTween('d', d => () => middleArcLine(d));
+      allSlices.select('path.main-arc').transition(transition)
+        .attrTween('d', d => () => state.arc(d))
+        .style('fill', d => colorOf(d.data, d.parent));
 
-    allSlices.select('.path-label')
-      .transition(transition)
+      allSlices.select('path.hidden-arc').transition(transition)
+        .attrTween('d', d => () => middleArcLine(d));
+
+      allSlices.select('.path-label')
+        .transition(transition)
         .styleTween('display', d => () => state.showLabels && textFits(d) ? null : 'none');
 
-    allSlices.selectAll('text.path-label textPath')
-      .text(d => nameOf(d.data));
+      allSlices.selectAll('text.path-label textPath')
+        .text(d => nameOf(d.data));
 
-    //
+      //
 
-    function middleArcLine(d) {
-      const halfPi = Math.PI/2;
-      const angles = [state.angleScale(d.x0) - halfPi, state.angleScale(d.x1) - halfPi];
-      const r = Math.max(0, (state.radiusScale(d.y0) + state.radiusScale(d.y1)) / 2);
+      function middleArcLine(d) {
+        const halfPi = Math.PI / 2;
+        const angles = [state.angleScale(d.x0) - halfPi, state.angleScale(d.x1) - halfPi];
+        const r = Math.max(0, (state.radiusScale(d.y0) + state.radiusScale(d.y1)) / 2);
 
-      if (!r || !(angles[1] - angles[0])) return '';
+        if (!r || !(angles[1] - angles[0])) return '';
 
-      const middleAngle = (angles[1] + angles[0]) / 2;
-      const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
-      if (invertDirection) { angles.reverse(); }
+        const middleAngle = (angles[1] + angles[0]) / 2;
+        const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+        if (invertDirection) { angles.reverse(); }
 
-      const path = d3Path();
-      path.arc(0, 0, r, angles[0], angles[1], invertDirection);
-      return path.toString();
-    }
-
-    function textFits(d) {
-      const deltaAngle = state.angleScale(d.x1) - state.angleScale(d.x0);
-      const r = Math.max(0, (state.radiusScale(d.y0) + state.radiusScale(d.y1)) / 2);
-      const perimeter = r * deltaAngle;
-      return nameOf(d.data).toString().length * CHAR_PX < perimeter;
-    }
-
-    function getNodeStack(d) {
-      const stack = [];
-      let curNode = d;
-      while (curNode) {
-        stack.unshift(curNode);
-        curNode = curNode.parent;
+        const path = d3Path();
+        path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+        return path.toString();
       }
-      return stack;
+
+      function textFits(d) {
+        const deltaAngle = state.angleScale(d.x1) - state.angleScale(d.x0);
+        const r = Math.max(0, (state.radiusScale(d.y0) + state.radiusScale(d.y1)) / 2);
+        const perimeter = r * deltaAngle;
+        return nameOf(d.data).toString().length * CHAR_PX < perimeter;
+      }
+
+      function getNodeStack(d) {
+        const stack = [];
+        let curNode = d;
+        while (curNode) {
+          stack.unshift(curNode);
+          curNode = curNode.parent;
+        }
+        return stack;
+      }
     }
   }
 });
